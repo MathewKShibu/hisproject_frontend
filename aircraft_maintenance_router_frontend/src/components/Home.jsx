9e951ff980330129a259081c30024faa95d635a6
MathewKShibu <mathewkshibu2001@gmail.com>
2023-11-24 09:49:40 +0530

    few changes

--- aircraft_maintenance_router_frontend/src/components/Home.jsx
+++ aircraft_maintenance_router_frontend/src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
 
   return (
     <div className="hometext" >
-      <img className="mainimage" src="./photos/amr4.webp" alt="flight image"/>
+      <img className="mainimage" src="./photos/amr3.png" alt="flight image"/>
       <h1 className="hometext">Aircraft Maintenance Router</h1>
     </div>
   );
